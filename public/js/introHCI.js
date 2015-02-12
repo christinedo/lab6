4528674809d36fab8bd54d90ac304d40adacaec7
Christine Do <cvd001@ucsd.edu>
2015-02-12 10:30:15 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,18 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, retrieveProject);
+	console.log("/project/" + idNumber);
+
+	function retrieveProject(result){
+		console.log(result);
+		var projectHTML = '<img src="' + result['image'] + '" class="detailsImage">' + '<h3>' 
+		+ result['title'] + '</h3>' + '<p><small>' + result['date'] + '</p></small>' + result['summary'];
+
+		$("#"+projectID).find(".thumbnail").find(".details").html(projectHTML);
+			console.log(projectHTML);
+	}	
 }
 
 /*
@@ -35,4 +47,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", retrieveColors);
+
+	function retrieveColors(result) {
+		var colors = result['colors']['hex'];
+
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	}
 }
